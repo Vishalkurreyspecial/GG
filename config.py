0e1ecbdd6d2f4538650d4c3e8051fd235a0441e8
Vishalkurreyspecial <vishalkurrey230@gmail.com>
2025-03-18 15:08:32 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Attack command template
+ATTACK_COMMAND = "./ok {target} {port} {time_duration} 900"
+
+# You can also add other configurations here
+BOT_TOKEN = "7140094105:AAHuXHDAjgEJEZQ7L0iFtXWr3JbYC3rwAoA"
+ADMIN_IDS = ["1662672529", "1662672529"]
+GROUP_ID = "-1002356850946"
+CHANNEL_USERNAME = "@fyyffgggvvvgvvcc"
